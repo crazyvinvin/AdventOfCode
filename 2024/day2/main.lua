e4b01b73b51671fc98a199782d0bf75c3e930bc4
Vincent Vermeire <vvermeire55@gmail.com>
2024-12-01 21:09:50 +0100

    setup lua for day2

---
+++ 2024/day2/main.lua
@@ -0,0 +1,5 @@
+package.path = package.path .. ";day2/src/?.lua"
+
+local module1 = require("module1")
+
+module1.hello()
---
+++ 2024/day2/src/module1.lua
@@ -0,0 +1,7 @@
+local M = {}
+
+function M.hello()
+    print("Hello from module1!")
+end
+
+return M
